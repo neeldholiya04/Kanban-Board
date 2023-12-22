b6bb9c951b67255b75f0d646123a407b9d15c1c8
Neel Dholiya <neeldholiya04@gmail.com>
2023-12-22 10:21:19 +0530

    delete functionality added

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -7,7 +7,7 @@ let rmBtn = document.querySelector(".rm-btn");
 let ticketCont = document.querySelector(".ticket-cont");
 let addModal = true;
 let taskColor = "r";
-
+let deleteMode = false;
 
 addBtn.addEventListener("click", function () {
   if (addModal) {
@@ -24,10 +24,10 @@ textArea.addEventListener("keydown", function (e) {
   if (key === "Enter") {
     // console.log("Generate Ticket");
     // console.log(textArea.value);
-    if(textArea.value == ""){
-        textArea.value = "";
-        alert("Please Enter Some Task!");
-        return;
+    if (textArea.value == "") {
+      textArea.value = "";
+      alert("Please Enter Some Task!");
+      return;
     }
     generateTicket(textArea.value);
     textArea.value = "";
@@ -35,15 +35,16 @@ textArea.addEventListener("keydown", function (e) {
     addModal = true;
   }
 });
-for (let i = 0; i < allPriorityColour.length; i++) {  
+for (let i = 0; i < allPriorityColour.length; i++) {
   allPriorityColour[i].addEventListener("click", function (t) {
-  for (let j = 0; j < allPriorityColour.length; j++) {
-    allPriorityColour[j].classList.remove("active");
-  }
-  allPriorityColour[i].classList.add("active");
-  taskColor = allPriorityColour[i].classList[1];
-  console.log(taskColor);    
-})}
+    for (let j = 0; j < allPriorityColour.length; j++) {
+      allPriorityColour[j].classList.remove("active");
+    }
+    allPriorityColour[i].classList.add("active");
+    taskColor = allPriorityColour[i].classList[1];
+    console.log(taskColor);
+  });
+}
 
 function generateTicket(task) {
   // <div class="ticket-cont">
@@ -62,11 +63,24 @@ function generateTicket(task) {
 
 // delete task
 rmBtn.addEventListener("click", function () {
-  let allTicket = document.querySelectorAll(".ticket-cont");
-  for (let i = 0; i<allTicket.length; i++){
-    allTicket[i].addEventListener("click", function(e){
-      e.currentTarget.remove();
-    })
-  }
-})
+  deleteMode = !deleteMode;
+  console.log(deleteMode);
 
+  if (deleteMode) {
+    console.log("Delete Mode On");
+    let allTicket = document.querySelectorAll(".ticket-cont");
+    for (let i = 0; i < allTicket.length; i++) {
+      allTicket[i].addEventListener("click", deleteTicket);
+    }
+  } else {
+    // Remove the event listeners when delete mode is turned off
+    let allTicket = document.querySelectorAll(".ticket-cont");
+    for (let i = 0; i < allTicket.length; i++) {
+      allTicket[i].removeEventListener("click", deleteTicket);
+    }
+  }
+});
+
+function deleteTicket(e) {
+  e.currentTarget.remove();
+}
